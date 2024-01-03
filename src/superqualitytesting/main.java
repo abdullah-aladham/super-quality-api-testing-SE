3bf9cbc2d44b0f40d515ccf0f9069a1b65dddcf0
abdullah-aladham <abdullah.aladham@gmail.com>
2024-01-03 14:59:30 +0200

    first commit

---
+++ src/superqualitytesting/main.java
@@ -0,0 +1,10 @@
+package superqualitytesting;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+	}
+
+}
