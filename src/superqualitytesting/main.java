0c785f27d5a4224b2081d3d1bd7b730b720658eb
abdullah-aladham <abdullah.aladham@gmail.com>
2024-01-03 15:31:47 +0200

    changed version of selenium for this proj from 4.16.1 to 3.14.2

--- src/superqualitytesting/main.java
+++ src/superqualitytesting/main.java
@@ -1,10 +1,15 @@
 package superqualitytesting;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+
 	}
 
 }
